6cb2e623ab0f45f4e9536b86a3c627d818209942
Antonio Carlos de Souza Zechin <antonio.zechin@b2wdigital.com>
2018-05-22 20:11:15 -0300

    cria excel

--- matriz.py
+++ matriz.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 from random import randint
+from datetime import date
 
 from pessoa_marked import PessoaMarkedForDeath
 
@@ -11,8 +12,9 @@ def criar_pessoas(quantity=151):
     for i in range(1, quantity):
         ano = str(randint(1900, 2000))
         mes = str(randint(1, 12))
-        dia = str(randint(1, 30))
+        dia = str(randint(1, 28))
         pm = PessoaMarkedForDeath("{}/{}/{}".format(ano, mes, dia))
+        pm.nome = "nome {}".format(i)
         yield pm
 
 def id_autoincrement(quantity=151):
@@ -31,14 +33,32 @@ def matriz():
     df = pd.DataFrame(data=d)
     return df
 
-def adiciona_15aniversarios(df):
+def adiciona_15aniversarios():
     """:return DataFrame com campo a mais de 15 aniversarios"""
+    df = matriz()
     df["idade+15"] = df["idade_atual"].apply(lambda x: x + 15)
     return df
 
-def adiciona_status_vida(df):
+def adiciona_status_vida():
     """:return DataFrame com campo de status de vida da pessoa"""
+    df = adiciona_15aniversarios()
     df["status_vida"] = df["classe_da_pessoa"].apply(lambda x: x.status)
     return df
 
-print adiciona_status_vida(adiciona_15aniversarios(matriz()))
+def calcula_data_nascimento():
+    """:return DataFrame com ano de nacimento de cada pessoa"""
+    df = adiciona_status_vida()
+    ano_atual = date.today().year
+    df["ano_nascimento"] = df["idade_atual"].apply(lambda x: ano_atual - x)
+    return df
+
+def cria_excel():
+    df = calcula_data_nascimento()
+    df["classe_da_pessoa"] = df["classe_da_pessoa"].apply(lambda x: x.nome)
+    writer = pd.ExcelWriter('pandas_simple.xlsx', engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+    print "excel criado"
+
+
+print cria_excel()
